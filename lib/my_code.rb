1f30f249a3c6039aa2f08723b3a81219c6b6a6d9
allison cortez <allizetorc@gmail.com>
2019-10-03 21:43:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+# def map(source_array)
+#   counter = 0
+#   while counter < source_array.length do
+#   yield(source_array[i]{|i|) counter * -1}
+#   end
+# end
+
+def map(source_array)
+  new_array = []
+  i = 0
+  while  i < source_array.length
+    new_array.push(yield(source_array[i]))
+    i =+ 1
+  end
+  new_array
+end
